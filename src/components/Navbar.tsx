2a97c5cf9acbb6a59191245492a81fd2a17ca12b
Fahad Hussain <162036149+MFahadHussain@users.noreply.github.com>
2025-07-24 17:58:50 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     >
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold text-theme-blue">
-          Fahad <span className="text-theme-purple">AI & Data Consultant</span>
+          Fahad <span className="text-theme-purple">Data & Ai Consultant</span>
         </Link>
 
         {/* Desktop Navigation */}
